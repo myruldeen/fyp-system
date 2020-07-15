107abe71562c07fdb116b36306a3f8fa69f6a815
myruldeen <funstudy10@gmail.com>
2020-07-15 19:22:11 +0800

    Update student_row.php

--- supervisor/student_row.php
+++ supervisor/student_row.php
@@ -8,7 +8,8 @@
                     LEFT JOIN course 
                     ON course.id=students.course_id 
                     LEFT JOIN supervisor
-                    ON supervisor.id = students.supervisor_id";
+                    ON supervisor.id = students.supervisor_id
+                    WHERE students.supervisor_id = '".$user['id']."'";
 		// $sql = "SELECT *, students.id AS studid FROM students LEFT JOIN course ON course.id=students.course_id WHERE students.id = '$id'";
 		$query = $conn->query($sql);
 		$row = $query->fetch_assoc();
